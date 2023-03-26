3d643d1ffadce49ea23aac3fb2537f46302f0cb2
Cissé <tairou.cisse@axazara.com>
2023-03-26 11:24:05 +0100

    - update hero and navbar

--- src/pages/components/Hero.jsx
+++ src/pages/components/Hero.jsx
@@ -1,25 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import Image from "next/image";
 
 export default function Hero() {
   return (
     <div className="w-full h-screen bg-[#f4f4f4]">
-      <div className="max-w-6xl flex flex-col mx-auto py-6 justify-between items-center md:items-center md:flex-row md:pt-16 md:pb-40 px-4">
-        <div className="md:max-w-2xl">
-          <p className="md:text-7xl text-3xl">
-            À la <span className="italic font-normal">recherche</span> d'un
-            nouveau <span className="font-bold">challenge.</span> Pour changer
-            le <span className="font-bold">monde</span> du
-            <span className="font-bold box-decoration-slice bg-[#f0ff00] ml-2">
-              digital.
-            </span>
-          </p>
-        </div>
-        <div>
-          <Image src="/assets/hero_image.webp" width={500} height={70} alt="" />
-        </div>
-      </div>
       <div className="container max-w-lg px-4 py-32 mx-auto mt-px text-left md:max-w-none md:text-center">
         <h1 className="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
           <span class="inline md:block">Start Crafting Your</span>{" "}
@@ -40,9 +24,6 @@ export default function Hero() {
             >
               Purchase Now
             </a>
-            <span className="absolute top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full">
-              only $15/mo
-            </span>
           </span>
           <a href="#_" className="mt-3 text-sm text-indigo-500">
             Learn More
--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import {
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+  AiOutlineFacebook,
+  AiOutlineMenu,
+} from "react-icons/ai";
 export default function Navbar() {
   return (
-    <div className="flex md:flex-row flex-col justify-between w-full px-10 items-center pt-2 bg-[#f4f4f4] pb-5">
-      <div className="text-md">
-        <span>Je suis Anna Franco</span>
-        <p className="text-gray-500">
-          Une{" "}
-          <span className="box-decoration-slice bg-[#f0ff00]">
-            activiste numérique,
-          </span>{" "}
-          rédactrice et consultante
-        </p>
-      </div>
-      <div className="flex gap-10 text-gray-600">
-        <div className="md:space-x-5 text-xl">
+    <div className="flex md:flex-row flex-col justify-between w-full px-20 items-center pt-6 bg-[#f4f4f4] pb-5">
+      <div className="text-md">Logo</div>
+      <div className="flex gap-5 text-gray-600 md:mt-0 mt-5">
+        <div className="space-x-5 text-xl">
           <Link className="hover:underline hover:text-fuchsia-500" href="/">
             Acceuil
           </Link>
@@ -25,7 +22,11 @@ export default function Navbar() {
             Contact
           </Link>
         </div>
-        <div>Medias sociaux logo</div>
+        <div className="flex items-center gap-5">
+          <AiOutlineTwitter size={25} />
+          <AiOutlineLinkedin size={25} />
+          <AiOutlineFacebook size={25} />
+        </div>
       </div>
     </div>
   );
